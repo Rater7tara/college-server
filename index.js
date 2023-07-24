2489baaf677e4c4d9d97b100d808a4820183dd42
rater7tara <nowshinakteremu005@gmail.com>
2023-07-24 21:01:23 +0600

    data show

--- index.js
+++ index.js
@@ -9,40 +9,64 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// MongoDB functions
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@nowshinkhan.c8ljhxf.mongodb.net/?retryWrites=true&w=majority`;
+const college = require('./data/college.json');
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+
+app.get('/', (req, res) =>{
+  res.send('college carts')
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+app.get('/college', (req, res) =>{
+  res.send(college);
+})
 
-    const collegeCollection = client.db('college').collection('carts');
-
-
-    app.get('/carts', async (req, res) => {
-        const result = await collegeCollection.find().toArray();
-        res.send(result); 
-    });
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+app.get('/college/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id)
+  if (id == 0) {
+      res.send(college)
   }
-}
-run().catch(console.dir);
+  else{
+      const selectedCollege = chefs.find(n => n.id == id);
+      res.send(selectedCollege)
+  }
+  
+})
+
+// // MongoDB functions
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@nowshinkhan.c8ljhxf.mongodb.net/?retryWrites=true&w=majority`;
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+
+//     const collegeCollection = client.db('college').collection('carts');
+
+
+//     app.get('/carts', async (req, res) => {
+//         const result = await collegeCollection.find().toArray();
+//         res.send(result); 
+//     });
+
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 
 app.get ('/', (req, res) =>{
